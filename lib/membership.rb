bc6f20eb32c80bd09a168239cdd3b7913ff2f61a
ayerest <irisiseve@gmail.com>
2019-07-21 12:35:35 -0700

    finished OO get swole lab

--- lib/membership.rb
+++ lib/membership.rb
@@ -1,7 +1,32 @@
 class Membership
-  attr_reader :cost
+  attr_reader :cost, :gym, :lifter
+  @@all = []
+  #trying out breaking gyms and lifters into class variables 
+  #for easier tracking
+  #UPDATE: turns out I didn't need these variables
+  #I was able to get all the info I needed using @@all 
+  @@gyms = []
+  @@lifters = []
 
-  def initialize(cost)
+  #trying out adding gym + lifter at initialization
+  def initialize(cost, gym, lifter)
     @cost = cost
+    @gym = gym
+    @lifter = lifter
+    @@all << self
+    @@gyms << gym
+    @@lifters << lifter
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.gyms
+    @@gyms
+  end
+
+  def self.lifters
+    @@lifters
   end
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,6 +3,13 @@
 require_relative '../config/environment.rb'
 
 # test code goes here
+lifter_one = Lifter.new("Ralph", 5000)
+lifter_two = Lifter.new("Sonny", 25)
+lifter_three = Lifter.new("Stephanie", 200)
+lifter_four = Lifter.new("Lucy", 300)
+
+gym_one = Gym.new("Gymm")
+gym_two = Gym.new("Test")
 
 binding.pry
 
